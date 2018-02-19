a57475b18f42b5b017ceabd82b05351d2a90102f
alejandro diaz gonzalez beltran <alejandrodgb@gmail.com>
2018-02-19 21:57:03 +1000

    Add function to write MXN rates for 366 days to csv

--- fxp_test.py
+++ fxp_test.py
@@ -1,19 +1,24 @@
-import forex_python as fxp
+import forex_python.converter as fxp
 import datetime
-from forex_python.converter import CurrencyRates
 import pandas as pd
 
 base_date = datetime.datetime.today()
 base_date = datetime.datetime(base_date.year, base_date.month, base_date.day)
 
 date_list = [base_date-datetime.timedelta(days = x) for x in range(366)]
+total = len(date_list)
 
-c = CurrencyRates()
+c = fxp.CurrencyRates()
+
+print('MXN to AUD rate: {}'.format(c.get_rate('AUD', 'MXN')))
 
 currency_list = []
+x=0
 for d in date_list:
+    x+=1
+    print('processing {} out of {}'.format(x, total))
     try:
-        rates_list = c.get_rates('MXN',d)
+        rates_list = c.get_rate('MXN',d)
         for r in rates_list:
             row = {}
             row['date'] = d
@@ -22,10 +27,6 @@ for d in date_list:
             currency_list.append(row)
     except Exception as e:
         print('Error: {} for day: {}'.format(e, d))
-    
-
-    
-
 
 pd.DataFrame(currency_list).to_csv('mxn.csv', index = False)
 
